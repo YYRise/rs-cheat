716e58f7ac95ddb8ca69464f9e4e17d8a9e8628b
YYRise <573849081@qq.com>
2023-09-18 18:39:22 +0800

    add str_usage.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod str_usage;
+use docopt::Docopt;
+
 fn main() {
     println!("Hello, world!");
+    let s = str_usage::usage();
+    // println!("{}", s);
+    let args = Docopt::new(s)
+        .and_then(|dopt| dopt.parse())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
+    println!("  init: {}, {}", args.get_str("--init"), args.get_bool("--init"));
 }
